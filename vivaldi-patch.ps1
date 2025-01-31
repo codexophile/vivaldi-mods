0dfc32d6609ca9ece4380ac5740ccdfc665ae349
Codexophile <jaliya.chirantha@gmail.com>
2025-01-31 09:44:54 +0530

    fix: Improve script formatting and enhance readability

--- vivaldi-patch.ps1
+++ vivaldi-patch.ps1
@@ -1,22 +1,24 @@
 # Author: debiedowner (original batch script)
 # PowerShell conversion
-
+. "C:\mega\IDEs\powershell\#lib\functions.ps1"
 # Make current directory work when run as administrator
+RunAsAdmin
 Set-Location -Path $PSScriptRoot
 
 $installPath = "C:\Program Files\Vivaldi\Application\"
 Write-Host "Searching at: $installPath"
 
 $latestVersionFolder = Get-ChildItem -Path $installPath -Recurse -File |
-    Where-Object { $_.Name -eq "window.html" } |
-    Select-Object -ExpandProperty DirectoryName -First 1
+Where-Object { $_.Name -eq "window.html" } |
+Select-Object -ExpandProperty DirectoryName -First 1
 
 if (-not $latestVersionFolder) {
-    Write-Host "Couldn't find the latest version folder."
-    Read-Host "Press Enter to exit"
-    exit
-} else {
-    Write-Host "Found latest version folder: '$latestVersionFolder'"
+  Write-Host "Couldn't find the latest version folder."
+  Read-Host "Press Enter to exit"
+  exit
+}
+else {
+  Write-Host "Found latest version folder: '$latestVersionFolder'"
 }
 
 $windowHtmlPath = Join-Path -Path $latestVersionFolder -ChildPath "window.html"
@@ -24,8 +26,8 @@ $windowBakHtmlPath = Join-Path -Path $latestVersionFolder -ChildPath "window.bak
 $customJsPath = Join-Path -Path $latestVersionFolder -ChildPath "custom.js"
 
 if (-not (Test-Path -Path $windowBakHtmlPath)) {
-    Write-Host "Creating a backup of your original window.html file."
-    Copy-Item -Path $windowHtmlPath -Destination $windowBakHtmlPath
+  Write-Host "Creating a backup of your original window.html file."
+  Copy-Item -Path $windowHtmlPath -Destination $windowBakHtmlPath
 }
 
 Write-Host "Copying js files to custom.js"
@@ -33,7 +35,7 @@ Get-Content -Path *.js | Set-Content -Path $customJsPath
 
 Write-Host "Patching window.html file"
 $content = Get-Content -Path $windowBakHtmlPath |
-    Where-Object { $_ -notmatch "</body>" -and $_ -notmatch "</html>" }
+Where-Object { $_ -notmatch "</body>" -and $_ -notmatch "</html>" }
 
 $content += "    <script src=`"custom.js`"></script>"
 $content += "  </body>"
